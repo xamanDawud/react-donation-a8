fcdeefda6349a81eef7ab9ad6ef96cdbc9540eab
xamanDawud <ahmeddaudking100@gmail.com>
2023-09-27 23:49:01 +0600

    redirects file added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,13 @@
 import "./Banner.css";
 const Banner = () => {
   return (
-    <div className="block text-center py-28 banner my-2 lg:pb-40 ">
+    <div className="block text-center lg:py-28 md:py-18 banner my-2 lg:pb-40 ">
       <h1 className="lg:text-3xl md:text-2xl font-bold my-6">
         I Grow By Helping People In Need
       </h1>
       <span>
         <input
-          className="border  border-gray-300 pe-12 ps-4 py-2 rounded-md rounded-e-none"
+          className="border  border-gray-300 pe-12 ps-4   py-2 rounded-md rounded-e-none"
           type="text"
           placeholder="Search"
         />
