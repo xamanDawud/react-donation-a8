1b78f4ab9ec3b0215b0b5f6fa07a70a3482fcf4d
xamanDawud <ahmeddaudking100@gmail.com>
2023-09-27 22:13:36 +0600

    conditional button added in all donation route

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -1,8 +1,10 @@
 import { getStoredJobApplication } from "../../Utility/localStorage";
 import { useLoaderData } from "react-router-dom";
 import DonationDisplay from "../DonationDisplay/DonationDisplay";
+import { useState } from "react";
 
 const Donation = () => {
+  let [data, setData] = useState(4);
   let donationAllData = useLoaderData();
   let getDonation = getStoredJobApplication();
   let filteredData = donationAllData.filter((data) =>
@@ -10,10 +12,22 @@ const Donation = () => {
   );
 
   return (
-    <div className="grid grid-cols-2 gap-6 m-12">
-      {filteredData.map((data) => (
-        <DonationDisplay key={data.Id} data={data}></DonationDisplay>
-      ))}
+    <div>
+      <div className="grid grid-cols-2 gap-6 m-12">
+        {filteredData.slice(0, data).map((data) => (
+          <DonationDisplay key={data.Id} data={data}></DonationDisplay>
+        ))}
+      </div>
+      <div className={`${data > 4 ? "hidden" : "block"}`}>
+        <button
+          onClick={() => {
+            setData(getDonation.length);
+          }}
+          className={`text-white bold px-2 py-2 rounded-md bg-sky-300 text-center block mx-auto`}
+        >
+          See All
+        </button>
+      </div>
     </div>
   );
 };
